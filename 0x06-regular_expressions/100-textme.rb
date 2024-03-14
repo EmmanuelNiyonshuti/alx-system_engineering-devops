ad69f8f37a6c46d5ec8dc003749ed8e69b806fc8
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-14 17:09:15 +0200

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+\d+)\] \[to:(\+\d+)\] \[flags:(.*?)\]/).join
+log = ARGV[0]
+
+matches = log.scan(/\[from:(\+\d+)\] \[to:(\+\d+)\] \[flags:(.*?)\]/)
+matches.each do |match|
+  sender = match[0]
+  receiver = match[1]
+  flags = match[2]
+  puts "#{sender},#{receiver},#{flags}"
+end
