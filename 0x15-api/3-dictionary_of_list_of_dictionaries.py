149799b7abbc805f6ec0817b94f0060df6d06d87
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-04 17:52:32 +0200

    fixed an error

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""""
-uses a RESTAPI 'https://jsonplaceholder.typicode.com/' for a given employee ID,
-returns information about his/her TODO list progress.
+"""
+Uses a RESTAPI 'https://jsonplaceholder.typicode.com/' for a given employee ID,
+returns information about their TODO list progress.
 """
 import json
 import requests
 
 if __name__ == "__main__":
-
     todos_url = 'https://jsonplaceholder.typicode.com/todos'
     users_url = 'https://jsonplaceholder.typicode.com/users'
 
@@ -15,27 +14,29 @@ if __name__ == "__main__":
     if users_r.status_code != 200:
         pass
     users_obj = users_r.json()
-    """ construct a dictionary so that I will get a username"""
     users_dict = {user['id']: user['username'] for user in users_obj}
 
     todos_r = requests.get(todos_url)
     if todos_r.status_code != 200:
         pass
     todos_obj = todos_r.json()
-    todos_list = []
+
+    """Create a dictionary to store tasks for each user ID"""
+    todos_dict = {}
     for todo in todos_obj:
-        for i, name in users_dict.items():
-            """check if the userId  in todos is the same as id in users
-            to get the corresponding username and id.
-            """
-            if todo.get('userId') == i:
-                username = name
-                user_id = i
-        todos_list.append({
+        user_id = todo.get('userId')
+        username = users_dict.get(user_id)
+        task = todo.get('title')
+        completed = todo.get('completed')
+
+        if user_id not in todos_dict:
+            todos_dict[user_id] = []
+
+        todos_dict[user_id].append({
             'username': username,
-            'task': todo.get('title'),
-            'completed': todo.get('completed')
+            'task': task,
+            'completed': completed
         })
-    todos_dict = {user_id: todos_list}
+
     with open('todo_all_employees.json', 'w') as json_f:
         json.dump(todos_dict, json_f)
