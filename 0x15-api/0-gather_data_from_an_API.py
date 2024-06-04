16b1e917f9d68922e279724bc5db2d18584c947d
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-04 10:51:35 +0200

    fix a requirement fail

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,19 +9,20 @@ import requests
 if len(sys.argv) != 2:
     sys.exit(1)
 
-employee_id = sys.argv[1]
+emp_id = sys.argv[1]
 
-user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-if user_response.status_code != 200:
+r = requests.get(f'https://jsonplaceholder.typicode.com/users/{emp_id}')
+if r.status_code != 200:
     sys.exit(1)
-user_info = user_response.json()
-employee_name = user_info.get('name')
+user_info = r.json()
+emp_name = user_info.get('name')
 
-todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-if todos_response.status_code != 200:
+r = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
+if r.status_code != 200:
     sys.exit(1)
-employee_todos = todos_response.json()
-completed_tasks = [task for task in employee_todos if task.get('completed')]
-
-print(f'Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(employee_todos)})')
-[print(f"\t {task.get('title')}") for task in completed_tasks]
+emp_todos = r.json()
+compl_tasks = [task for task in emp_todos if task.get('completed')]
+num_todos = len(emp_todos)
+num_compl_tasks = len(compl_tasks)
+print(f'Employee {emp_name} is done with tasks({num_compl_tasks}/{num_todos})')
+[print(f"\t {task.get('title')}") for task in compl_tasks]
