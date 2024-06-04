a228a202e399bca22bfa3e5b8bd02556e9539e88
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-04 11:59:31 +0200

    fixed formating style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ returns information about his/her TODO list progress.
 import requests
 import sys
 
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
@@ -13,20 +14,21 @@ if __name__ == "__main__":
 
     emp_id = sys.argv[1]
 
-    url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
-    response = requests.get(url)
+    users_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
+    response = requests.get(users_url)
     if response.status_code != 200:
         sys.exit(1)
     user_info = response.json()
     emp_name = user_info.get('name')
-
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}'
-    response = requests.get(url)
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}'
+    response = requests.get(todos_url)
     if response.status_code != 200:
         sys.exit(1)
     emp_todos = response.json()
     compl_tasks = [task for task in emp_todos if task.get('completed') is True]
-    n_todos = len(emp_todos)
-    n_compl_tasks = len(compl_tasks)
-    print(f'Employee {emp_name} is done with tasks({n_compl_tasks}/{n_todos})')
+    todos_done = len(emp_todos)
+    todos_count = len(compl_tasks)
+    print("Employee {} is done with tasks({}/{}):".format(
+        emp_name, todos_count, todos_done))
+
     [print(f"\t {task.get('title')}") for task in compl_tasks]
